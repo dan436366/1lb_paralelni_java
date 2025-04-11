1ced383a81c5a49923dda1e84a44aeac74120860
dan436366 <mavpadan@gmail.com>
2025-04-11 15:18:45 +0300

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,73 +1,107 @@
 package org.example;
 
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         System.out.print("Enter number of threads: ");
-        int numThreads = scanner.nextInt();
+        int threadCount = scanner.nextInt();
 
-        System.out.print("Enter step of threads: ");
-        int step = scanner.nextInt();
+        int[] steps = new int[threadCount];
+        int[] delays = new int[threadCount];
 
-        Worker[] workers = new Worker[numThreads];
-        Thread[] threads = new Thread[numThreads];
-
-        for (int i = 0; i < numThreads; i++) {
-            workers[i] = new Worker(i + 1, step);
-            threads[i] = new Thread(workers[i]);
-            threads[i].start();
+        for (int i = 0; i < threadCount; i++) {
+            steps[i] = random.nextInt(5) + 1;
+            delays[i] = (random.nextInt(5) + 1) * 1000;
         }
 
-        new Thread(() -> {
-            for (int i = 0; i < numThreads; i++) {
-                try {
-                    Thread.sleep(1000);
-                    workers[i].stop();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+        SumThread[] workers = new SumThread[threadCount];
+
+        for (int i = 0; i < threadCount; i++) {
+            workers[i] = new SumThread(i + 1, steps[i]);
+            workers[i].start();
+        }
+
+        ControllerThread controller = new ControllerThread(workers, delays);
+        controller.start();
+
+        scanner.close();
     }
 }
 
-class Worker implements Runnable {
-    private final int id;
-    private final int step;
-    private boolean running = true;
+class ControllerThread extends Thread {
+    private final SumThread[] threads;
+    private final int[] delays;
 
-    private long sum = 0;
-    private int count = 0;
-
-    public Worker(int id, int step) {
-        this.id = id;
-        this.step = step;
+    public ControllerThread(SumThread[] threads, int[] delays) {
+        this.threads = threads;
+        this.delays = delays;
     }
 
-    public void stop() {
-        running = false;
+    @Override
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        boolean[] stopped = new boolean[threads.length];
+
+        while (true) {
+            long currentTime = System.currentTimeMillis();
+            boolean allStopped = true;
+
+            for (int i = 0; i < threads.length; i++) {
+                if (!stopped[i]) {
+                    allStopped = false;
+                    if (currentTime - startTime >= delays[i]) {
+                        threads[i].stopRunning();
+                        stopped[i] = true;
+                    }
+                }
+            }
+
+            if (allStopped) {
+                break;
+            }
+        }
+
+        for (SumThread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.err.println("Cant wait to close thread");
+            }
+        }
+    }
+}
+
+class SumThread extends Thread {
+    private final int id;
+    private final int step;
+    volatile private boolean running = true;
+
+    private long sum = 0;
+    private long count = 0;
+
+    public SumThread(int id, int step) {
+        this.id = id;
+        this.step = step;
     }
 
     @Override
     public void run() {
         int current = 0;
-
         while (running) {
             sum += current;
-            count++;
             current += step;
-
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            count++;
         }
+        System.out.printf("Thread #%d sum = %d, addends = %d%n", id, sum, count);
+    }
 
-        System.out.printf("Thread %d : sum = = %d, addends = %d\n", id, sum, count);
+    public void stopRunning() {
+        running = false;
     }
 }
+
